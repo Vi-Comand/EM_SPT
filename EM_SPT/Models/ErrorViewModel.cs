0c0ceaad8940b7dbb62e4d2e2cf839aa0aa45353
kravchenko_v_e <kravcenko.vi@live.com>
2019-07-17 12:14:20 +0300

    Добавьте файлы проекта.

---
+++ EM_SPT/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EM_SPT.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
